a1adbf77b0f4c1af797f1f8ec0ae023af4eb3fac
Daniela <driascos@unicauca.edu.co>
2024-04-13 09:57:52 -0500

    Add atr,setter,getter Tipo pregunta, dept and tel

--- src/main/java/co/edu/unicauca/asstproject/project_asst/models/TipoPregunta.java
+++ src/main/java/co/edu/unicauca/asstproject/project_asst/models/TipoPregunta.java
@@ -1,5 +1,22 @@
 package co.edu.unicauca.asstproject.project_asst.models;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name="TipoPregunta")
 public class TipoPregunta {
+    @Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer idtippregunta;
+    private String nombre;
+    private String descripcion;
     
 }
