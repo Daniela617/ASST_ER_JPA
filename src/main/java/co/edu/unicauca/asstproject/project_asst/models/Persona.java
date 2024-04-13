21d5088e112d9897698974651cc06f0be08db0f2
Daniela <driascos@unicauca.edu.co>
2024-04-13 15:35:40 -0500

    Add persona

--- src/main/java/co/edu/unicauca/asstproject/project_asst/models/Persona.java
+++ src/main/java/co/edu/unicauca/asstproject/project_asst/models/Persona.java
@@ -1,5 +1,32 @@
 package co.edu.unicauca.asstproject.project_asst.models;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Persona {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int idpersona;
+    private String tipoidentificacion;
+    private String numeroidentificacion;
+    private String nombres;
+    private String apellidos;
+
+    public Persona(int idpersona){
+        this.idpersona=idpersona;
+    }
 }
